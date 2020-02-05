c496eb42c4aff3101fd03b86932622b1dcfb47e9
Sergey Li <pincats@gmail.com>
2020-02-06 00:11:33 +0300

    feat: Add image slider to app bar

--- app/src/main/java/com/example/android/tourguide/DetailsActivity.java
+++ app/src/main/java/com/example/android/tourguide/DetailsActivity.java
@@ -5,11 +5,22 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.viewpagerindicator.CirclePageIndicator;
+
+import java.util.ArrayList;
 
 public class DetailsActivity extends AppCompatActivity {
+
+    private static ViewPager mPager;
+    private static int currentPage = 0;
+    private static int NUM_PAGES = 0;
+    private ArrayList<Integer> imageModelArrayList;
+
+    private int[] myImageList = new int[]{R.drawable.cake_image, R.drawable.ryba, R.drawable.terrasa};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +42,60 @@ public class DetailsActivity extends AppCompatActivity {
         GuideCard card = (GuideCard) getIntent().getSerializableExtra("EXTRA_PLACE_OBJECT");
 
         this.setTitle(card.getTitle());
+
+        imageModelArrayList = new ArrayList<>();
+        imageModelArrayList = populateList();
+
+        init();
     }
+
+    private ArrayList<Integer> populateList() {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (Integer imageResourceId : myImageList) {
+            list.add(imageResourceId);
+        }
+
+        return list;
+    }
+
+    private void init() {
+
+        mPager = findViewById(R.id.pager);
+        mPager.setAdapter(new SlidingImageAdapter(DetailsActivity.this, imageModelArrayList));
+
+        CirclePageIndicator indicator = findViewById(R.id.indicator);
+
+        indicator.setViewPager(mPager);
+
+        final float density = getResources().getDisplayMetrics().density;
+
+        //Set circle indicator radius
+        indicator.setRadius(5 * density);
+
+        NUM_PAGES = imageModelArrayList.size();
+
+        // Pager listener over indicator
+        indicator.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageSelected(int position) {
+                currentPage = position;
+
+            }
+
+            @Override
+            public void onPageScrolled(int pos, float arg1, int arg2) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int pos) {
+
+            }
+        });
+
+    }
+
 }
